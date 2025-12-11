162d83cafc8a8806b679d896c0a2b2bf79ab8248
Garrett <garrettwells1122@gmail.com>
2025-12-10 21:34:42 -0500

    add day 2 part 2

--- 2025/day_2/main.go
+++ 2025/day_2/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 
+	"github.com/GarrettWells/AdventOfCode/util"
 	"github.com/joho/godotenv"
 )
 
@@ -29,8 +31,38 @@ func part1(input string) int {
 	return sum
 }
 
+func part2(input string) int64 {
+	var sum int64 = 0
+	for _, line := range strings.Split(input, ",") {
+		ranges := strings.Split(line, "-")
+		start, _ := strconv.Atoi(ranges[0])
+		end, _ := strconv.Atoi(ranges[1])
+
+		for i := start; i <= end; i++ {
+			i_str := strconv.Itoa(i)
+
+			if len(i_str) == 1 {
+				continue
+			}
+
+			for k := 0; k <= len(i_str); k++ {
+				count := strings.Count(i_str, i_str[0:k])
+				if k*count == len(i_str) && count > 1 {
+					sum += int64(i)
+					fmt.Println(i_str)
+					break
+				}
+			}
+
+		}
+	}
+	return sum
+}
+
 func main() {
 	godotenv.Load(".env")
-	// input := util.GetInput("https://adventofcode.com/2025/day/2/input", os.Getenv("session_cookie"))
-	fmt.Println(part1("4440-4500,5555-5555"))
+	input := util.GetInput("https://adventofcode.com/2025/day/2/input", os.Getenv("session_cookie"))
+	fmt.Println(part2(input))
+	// fmt.Println(part2("824824821-824824827"))
+	// fmt.Println(part2("11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"))
 }
